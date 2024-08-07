129df644320c047713517a036a9b028b0eafe511
Natnael <natibir400@gmail.com>
2024-08-07 22:43:07 +0300

    restored

---
+++ ToDo with TS/README.md
@@ -0,0 +1,65 @@
+# My TO-DO-LIST
+
+This is a simple To-Do List web application that allows users to add,edit,view and manage their tasks. It also includes a theme switcher to toggle between modes.
+
+![alt text](<screenshots/Screenshot 2024-08-05 at 1.52.39 in the afternoon.png>)
+
+## Features
+
+- **Add Tasks**: Users can add tasks with a name and description.
+- **View Tasks**: Tasks are displayed in a list format.
+- **Edit Tasks**: Users can edit description and title of tasks.
+- **Manage Tasks**: Users can mark tasks as completed or delete them.
+- **Theme Switcher**: Toggle between themes.
+
+## Installation
+
+1. **Clone the repository:**
+
+   ```bash
+   git clone https://github.com/Natib22/A2SV-project-phase.git
+   ```
+
+2. **Navigate to the ToDo App directory:**
+
+   ```bash
+   cd A2SV-project-phase/ToDo-with-TS
+   ```
+
+3. **Build the typescript:**
+
+   ```bash
+   npx tsc todo.ts
+   ```
+
+4. **Open `index.html` in your browser:**
+
+   Double-click the `index.html` file or open it using your preferred web browser.
+
+## Usage
+
+1. **Add a Task:**
+
+   - Enter the task name and description in the input fields.
+   - Click the `+` button to add the task to the list.
+
+2. **Manage Tasks:**
+
+   - Once a task is added, you can:
+     - Mark it as completed by clicking the `Completed` button.
+     - Delete the task by clicking the `Delete` button.
+
+3. **Theme Switcher:**
+   - Click the theme button to toggle between themes.s
+
+![alt text](<screenshots/Screenshot 2024-08-05 at 1.53.12 in the afternoon.png>)
+
+## Project Structure
+
+- `index.html`: The main HTML file containing the structure of the app.
+- `style.css`: The CSS file for styling the application.
+- `todo.ts`: The JavaScript file containing the functionality for adding, completing, and deleting tasks, as well as the theme switcher.
+
+## Credits
+
+- **Author**: Natnael Biruk
---
+++ ToDo with TS/todo.ts
@@ -0,0 +1,154 @@
+const taskName = document.getElementById("taskName") as HTMLInputElement;
+const taskDescription = document.getElementById("taskDescription") as HTMLInputElement;
+const container = document.getElementById("taskContainer");
+
+var dropdownBtn = document.querySelector(".themeButton");
+var dropdownContent = document.getElementById("dropdown-content");
+var themeIcon = document.getElementById("themeIcon");
+
+
+function addTask() {
+  if (taskName?.value === "" || taskDescription?.value === "") {
+    alert("Empty Fields enter something");
+  } else {
+    let myDiv = document.createElement("div");
+    myDiv.className = "taskList";
+
+    let innerDiv = document.createElement("div");
+    innerDiv.className = "parag";
+
+    let taskTitle = document.createElement("h2");
+    taskTitle.textContent = taskName.value;
+
+    let mySpan = document.createElement("span");
+    mySpan.textContent = taskDescription.value;
+
+    let edit = document.createElement("button");
+    edit.className = "completed";
+    edit.id = "edit";
+    edit.textContent = "edit";
+
+    let complete = document.createElement("button");
+    complete.className = "completed";
+    complete.textContent = "Completed";
+
+    let dele = document.createElement("button");
+    dele.className = "delete";
+    dele.textContent = "Delete";
+
+    complete.addEventListener("click", function () {
+      completeButton(myDiv, taskTitle, mySpan, complete, edit);
+    });
+    dele.addEventListener("click", function () {
+      deleteButton(myDiv);
+    });
+
+    edit.addEventListener("click", function () {
+      editButton(taskTitle, mySpan);
+    });
+
+    innerDiv.append(taskTitle);
+    innerDiv.append(mySpan);
+
+    myDiv.append(innerDiv);
+    myDiv.append(edit);
+    myDiv.append(complete);
+    myDiv.append(dele);
+
+    container?.append(myDiv);
+
+    // Clear input fields
+    taskName.value = "";
+    taskDescription.value = "";
+    saveData();
+  }
+}
+
+function deleteButton(myDiv: Element) {
+  if (container) {
+    container.removeChild(myDiv);
+    saveData();
+  }
+}
+
+function completeButton(myDiv: Element, taskTitle: HTMLHeadingElement | null, mySpan: HTMLSpanElement | null, complete: Element , edit: Element) {
+  if (mySpan) {
+    mySpan.style.textDecoration = "line-through";
+  }
+  if (taskTitle) {
+    taskTitle.style.textDecoration = "line-through";
+  }
+  myDiv.removeChild(complete);
+  myDiv.removeChild(edit);
+  saveData();
+}
+
+function saveData() {
+  if (container) {
+    localStorage.setItem("myList", container.innerHTML);
+  }
+}
+
+function showData() {
+  if (container) {
+    container.innerHTML = localStorage.getItem("myList") || "";
+    attachEventListeners();
+  }
+}
+
+function attachEventListeners() {
+  const tasks = container?.querySelectorAll(".taskList");
+  tasks?.forEach((task) => {
+    const complete = task.querySelector(".completed");
+    const dele = task.querySelector(".delete");
+    const edit = task.querySelector("#edit");
+    const taskTitle = task.querySelector("h2");
+    const mySpan = task.querySelector("span");
+
+    if (complete && edit) {
+      complete.addEventListener("click", function () {
+        completeButton(task, taskTitle, mySpan, complete , edit);
+      });
+    }
+
+    if (edit) {
+      edit.addEventListener("click", function () {
+        editButton(taskTitle, mySpan);
+      });
+    }
+
+    if (dele) {
+      dele.addEventListener("click", function () {
+        deleteButton(task);
+      });
+    }
+  });
+}
+
+showData();
+
+var parag = document.getElementById("parag");
+
+function changeToBlue() {
+  const root = document.documentElement;
+  root.classList.toggle("light-blue"); // Toggle the 'light-blue' class on the :root element
+  document.body.style.animation = "AnimationName 59s ease infinite;";
+  const themeIcon = document.getElementById("themeIcon")
+  if (themeIcon)
+  themeIcon.style.filter = "hue-rotate(180deg) brightness(1.2)";
+}
+
+function change() {
+    changeToBlue();
+  
+}
+
+function editButton(taskTitle: HTMLHeadingElement | null, mySpan: HTMLSpanElement | null) {
+    if (taskTitle){
+        taskTitle.textContent = prompt("Task Name");
+
+    }
+  if (mySpan)
+    mySpan.textContent = prompt("Task description");
+  saveData();
+}
